89f0ba853c3a2597c11be944eb8baf490f8dcb77
nikhilreddy2002 <nikhil2002s@gmail.com>
2022-02-21 03:20:56 +0530

    The code is overwritten multiple times

--- LAB2/Excercise_21.s
+++ LAB2/Excercise_21.s
@@ -1,12 +1,12 @@
-	AREA Excercise2_1, CODE, READONLY
-	EXPORT RESET_HANDLER2_1
-RESET_HANDLER2_1
+	AREA Excercise, CODE, READONLY
+	EXPORT RESET_HANDLER
+RESET_HANDLER
 
-START	MOV R1, #4294967295
-		MOV R2, #1
-		ADD R3, R1, R2
-		ADDS R4, R1, R2
-		ADC R5, R1, R2
-		MOV R2, #231
-STOP 	B STOP 
+START	LDR R1, =0x000004AF; R1,R2 together form one 64 bit number(MSB is in R1)
+		LDR R2, =0x32ADBC14; note here numbers are represented using 2’s complement  
+		LDR R3, =0xAFF123F0; R3,R4 together form another 64 bit number(MSB is in R3)
+		LDR R4, =0xF1264923;
+		RSBS R6, R4, R2; sub the least significant words
+		RSC R5, R3, R1; sub the most significant words
+STOP 	B STOP ; R6,R5 together form one 64 bit sum (MSB is in R6)
 		END
